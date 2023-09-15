befd3c61334ddc338e2c0b532a68ded2cd30bf4a
Spencer Le <lespencer88@gmail.com>
2023-09-14 21:15:08 -0500

    Style 2 and Toggle Button
    Created style2.css that edits classes for all pages. Created functions.js to control a toggle button for style1.css and style2.css.

---
+++ functions.js
@@ -0,0 +1,21 @@
+function toggle() {
+    const stylesheet = document.getElementById('style');
+    if (stylesheet.href.includes('style1.css')) {
+        stylesheet.href = 'style2.css'; 
+        localStorage.setItem('stylesheet', 'style2.css'); 
+    } else {
+        stylesheet.href = 'style1.css'; 
+        localStorage.setItem('stylesheet', 'style1.css'); 
+    }
+}   
+
+window.onload = function(){
+    const savedStylesheet = localStorage.getItem('stylesheet');
+    if (savedStylesheet) {
+        const stylesheet = document.getElementById('style');
+        stylesheet.href = savedStylesheet;
+    } else {
+        stylesheet.href = 'style1.css';
+        localStorage.setItem('stylesheet', 'style1.css');
+    }
+}
